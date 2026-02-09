32edcf6197474ed32407e1f564a0be3b170a64d6
nemu nemu <70689022+darkside-rat@users.noreply.github.com>
2026-02-09 11:17:06 +0300

    Fixes

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -8,24 +8,15 @@ repositories {
 }
 
 dependencies {
-    // Minecraft / mappings
     minecraft("com.mojang:minecraft:1.21.8")
     mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
 
-    // Loader + Kotlin language
     modImplementation("net.fabricmc:fabric-loader:0.16.9")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.13.9+kotlin.2.3.10")
 }
 
 loom {
-    // JVM toolchain для Kotlin и Java синхронизируются здесь
     java {
         toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "21"
     }
 }
