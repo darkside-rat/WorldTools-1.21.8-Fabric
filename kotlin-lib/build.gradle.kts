dcf50ac1c841e6b678b40b879f36b1156711d159
nemu nemu <70689022+darkside-rat@users.noreply.github.com>
2026-02-11 02:45:24 +0300

    Minor changes

--- kotlin-lib/build.gradle.kts
+++ kotlin-lib/build.gradle.kts
@@ -14,8 +14,11 @@ dependencies {
     minecraft("com.mojang:minecraft:${rootProject.property("minecraft_version")}")
     mappings("net.fabricmc:yarn:${rootProject.property("yarn_mappings")}:v2")
 
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.property("cloth_config_version")}")
     modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.property("fabric_api_version")}")
     
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.1")
 }
