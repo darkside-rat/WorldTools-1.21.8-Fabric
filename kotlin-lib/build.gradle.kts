6554e0f6d061a2f4d07f298e389c10338c4bc2a3
nemu nemu <70689022+darkside-rat@users.noreply.github.com>
2026-02-10 21:06:10 +0300

    Fixing

--- kotlin-lib/build.gradle.kts
+++ kotlin-lib/build.gradle.kts
@@ -10,12 +10,6 @@ kotlin {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:${rootProject.property("minecraft_version")}")
-    mappings("net.fabricmc:yarn:${rootProject.property("yarn_mappings")}:v2")
-
-    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("loader_version")}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.property("fabric_version")}")
-
     implementation(kotlin("stdlib"))
 
     api(project(":kotlin-lib"))
